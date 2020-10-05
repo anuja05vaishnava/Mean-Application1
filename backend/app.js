098a6d51086f06c67dc96bb6ddb45d06110c6695
anuja05vaishnava <60468761+anuja05vaishnava@users.noreply.github.com>
2020-10-05 18:50:24 +0530

    backend

---
+++ backend/app.js
@@ -0,0 +1,38 @@
+const express = require("express");
+const bodyParser = require("body-parser");
+const mongoose = require("mongoose");
+
+const postRoutes = require("./routes/posts");
+
+const app = express();
+
+mongoose.connect("mongodb+srv://anuja:jbZJmPH5shroUu7v@cluster0.rjphj.mongodb.net/node-angular?retryWrites=true&w=majority")
+.then(() =>
+{
+  console.log('Connected');
+})
+.catch(() => {
+  console.log('Error');
+} );
+
+
+app.use(bodyParser.json());
+app.use(bodyParser.urlencoded({ extended: false }));
+
+app.use((req, res, next) => {
+  res.setHeader("Access-Control-Allow-Origin", "*");
+  res.setHeader(
+    "Access-Control-Allow-Headers",
+    "Origin, X-Requested-With, Content-Type, Accept"
+  );
+  res.setHeader(
+    "Access-Control-Allow-Methods",
+    "GET, POST, PATCH, PUT, DELETE, OPTIONS"
+  );
+  next();
+});
+
+app.use("/api/posts",postRoutes);
+
+
+module.exports = app;
